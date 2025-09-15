7700702caaaa80ad346f7132fe391bf03d49b39f
Grecu Alexandru Gabriel <grecualexandrugabriel>
2025-09-16 01:26:24 +0300

    adding ci-cd to the project

--- backend/src/main/java/com/example/carins/CarInsuranceApplication.java
+++ backend/src/main/java/com/example/carins/CarInsuranceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarInsuranceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CarInsuranceApplication.class, args);
     }
